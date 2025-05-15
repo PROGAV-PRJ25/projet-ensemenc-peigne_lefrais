259adff0502d5ac4f6b1974146441701934dac7a
louisonpgn <lpeigne@ensc.fr>
2025-05-15 09:58:11 +0200

    correction pour que ça aille avec terrain

--- ProjetPotager/Plantes.cs
+++ ProjetPotager/Plantes.cs
@@ -119,7 +119,7 @@ public class CitronVert : Plante
 
 public class Cocotier : Plante
 {
-    public Cocotier() : base("Cocotier", "Vivace", "Mars", "Bord de mer, sable pauvre, soleil, vent salé", TypeTerrain.SableMarin,
+    public Cocotier() : base("Cocotier", "Vivace", "Mars", "Bord de mer, sable pauvre, soleil, vent salé", TypeTerrain.SableuxAvecEau,
                    80, 750, "Très lente (7–10 ans)", 1460, 90, 30, "Bipolaris", 100, 35)
     {
     }
